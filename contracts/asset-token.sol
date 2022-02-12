0d51fba99610588953246006490ef573cd7c41ad
Clark <clark@contxts.io>
2022-02-12 10:59:13 +0900

    Initial commit

---
+++ contracts/asset-token.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.8.11;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AssetToken is ERC20 {
+    constructor(
+        uint256 totalSupply_,
+        string memory name_,
+        string memory symbol_
+    ) ERC20(name_, symbol_) {
+        _mint(msg.sender, totalSupply_);
+    }
+}
